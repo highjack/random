ee4c368c067bb63bb6fbcb38b4a3132a5bacf696
highjack <highjack@users.noreply.github.com>
2023-11-29 13:16:45 +0000

    Update entrypoint.sh

--- ps-scan/entrypoint.sh
+++ ps-scan/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-echo Downloading $PACKAGE $VERSION
+echo [+] Downloading $PACKAGE $VERSION
 mono /usr/bin/nuget.exe install $PACKAGE -Version $VERSION -OutputDirectory /packages -Source https://www.powershellgallery.com/api/v2/
-chmod 777 -R /packages
+echo [+] Grepping for potentially dangerous functionality
+rm /results/* -r 2>/dev/null
+rm -r ./nupkg 2>/dev/null
+mkdir ./nupkg
+find . -wholename "./packages/*.nupkg" -exec cp {} ./nupkg/ \;
+zip -r $PACKAGE-$VERSION.zip ./nupkg
+cp $PACKAGE-$VERSION.zip /results
+grep -iEHnr --include='*.ps1' --include='*.psm1' -f /regexes /packages >> /results/$PACKAGE-$VERSION-report.txt
+echo Done!
