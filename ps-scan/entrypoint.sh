b25b98e9db43dc139c44227fa65e9d7f2f323e2f
highjack <highjack@users.noreply.github.com>
2023-11-29 12:06:18 +0000

    Update entrypoint.sh

--- ps-scan/entrypoint.sh
+++ ps-scan/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo Downloading $PACKAGE $VERSION
 mono /usr/bin/nuget.exe install $PACKAGE -Version $VERSION -OutputDirectory /packages -Source https://www.powershellgallery.com/api/v2/
+chmod 777 -R /packages
